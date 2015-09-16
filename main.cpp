ad711662584f6d937396e54ee680ede5b6d67775
Roland Leißa <leissa@cs.uni-saarland.de>
2015-09-16 14:52:40 +0200

    fixed memory leak if excpetion is thrown
    * by using std::unique_ptr World will be deleted even if an exception is thrown
    * I also changed World* to World& to make everything more concise

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
+#include <memory>
+
 #include "world.h"
 
 using namespace henk;
 
-Lambda poly_id(World* world, std::string tvar, std::string var) {
-    auto type_lambda = world->lambda(tvar, world->get_prim_const("*"));
-    auto id_lambda = world->lambda(var, type_lambda->var());
+Lambda poly_id(World& world, std::string tvar, std::string var) {
+    auto type_lambda = world.lambda(tvar, world.get_prim_const("*"));
+    auto id_lambda = world.lambda(var, type_lambda->var());
     id_lambda->close(id_lambda->var());
     type_lambda->close(id_lambda);
 
     return type_lambda;
 }
 
-void test1(World* world) {
+void test1(World& world) {
     auto type_lambda = poly_id(world, "a", "x");
 
     auto atype = type_lambda->inftype();
@@ -22,24 +24,24 @@ void test1(World* world) {
     std::cout << std::endl;
 }
 
-void test2(World* world) {
+void test2(World& world) {
      // u = lambda y:* . Int
     // (lambda x: (u Bool). 42) (Int)
     
-    auto u = world->lambda("y", world->get_prim_const("*"));
-    u->close(world->get_prim_const("Int"));
+    auto u = world.lambda("y", world.get_prim_const("*"));
+    u->close(world.get_prim_const("Int"));
     
-    auto lambda = world->lambda("x", world->app(
-        u, world->get_prim_const("Bool")
+    auto lambda = world.lambda("x", world.app(
+        u, world.get_prim_const("Bool")
         )
     );
-    lambda->close(world->literal(42));
+    lambda->close(world.literal(42));
     
-    world->add_external(lambda);
-    world->cleanup();
-    //world->show_expressions();
+    world.add_external(lambda);
+    world.cleanup();
+    //world.show_expressions();
 
-    auto app = world->app(lambda, world->literal(33));//get_prim_const("Int"));
+    auto app = world.app(lambda, world.literal(33));//get_prim_const("Int"));
     std::cout << app->non_reduced_repr() << " reduced to ";
     app.dump();
     auto tapp = app->inftype();
@@ -48,22 +50,22 @@ void test2(World* world) {
     std::cout << std::endl;
 }
 
-void test3(World* world) {
+void test3(World& world) {
      // f (forall b. b -> b)
     // where f: forall a. a -> Int
     // should fail in predicative system
-    auto f = //world->pi("α", world->get_prim_const("*"));
-        world->lambda("α", world->get_prim_const("*"));
-    auto inf = world->lambda("x", f->var());
-    inf->close(world->literal(42));
+    auto f = //world.pi("α", world.get_prim_const("*"));
+        world.lambda("α", world.get_prim_const("*"));
+    auto inf = world.lambda("x", f->var());
+    inf->close(world.literal(42));
     f->close(inf);
 
-    /*f->close(world->fun_type(
-            f->var(), world->get_prim_const("Int")
+    /*f->close(world.fun_type(
+            f->var(), world.get_prim_const("Int")
         )
     );*/
-    auto forallb = world->pi("β", world->get_prim_const("*"));
-    forallb->close(world->fun_type(
+    auto forallb = world.pi("β", world.get_prim_const("*"));
+    forallb->close(world.fun_type(
         forallb->var(), forallb->var()
         )
     );
@@ -78,7 +80,7 @@ void test3(World* world) {
     forallb->inftype().dump();
     
     std::cout << std::endl;
-    auto app = world->app(f, forallb);
+    auto app = world.app(f, forallb);
     std::cout << "f g = ";
     app.dump();
     std::cout << std::endl;
@@ -90,9 +92,9 @@ void test3(World* world) {
 
 }
 
-void test4(World* world) {
-    auto i42 = world->literal(42);
-    auto i42prim = world->literal(42);
+void test4(World& world) {
+    auto i42 = world.literal(42);
+    auto i42prim = world.literal(42);
     std::cout << "created two numbers 42 and their physical addresses are ";
     std::cout << *i42 << " and " << *i42prim << std::endl;
     assert(*i42 == *i42prim && "number 42 have different addresses");
@@ -108,42 +110,42 @@ void test4(World* world) {
     assert(*id1 == *id2 && "id functions have different addresses");
 }
 
-void test5(World* world) {
-    auto singl = world->tuple(std::vector<Def> {world->literal(42)});
+void test5(World& world) {
+    auto singl = world.tuple(std::vector<Def> {world.literal(42)});
     singl.dump();
     std::cout << ": ";
     singl->inftype().dump();
-    auto p = world->tuple(std::vector<Def> {world->literal(23), singl});
+    auto p = world.tuple(std::vector<Def> {world.literal(23), singl});
     std::cout << std::endl;
     p.dump();
     std::cout << ": ";
     p->inftype().dump();
     
-    auto p2 = world->extract(p, 1);
+    auto p2 = world.extract(p, 1);
     std::cout << std::endl;
     p2.dump();
 }
 
 int main(int argc, char* argv[]) {
-    auto world = new World();
+    std::unique_ptr<World> world(new World());
     world->dump_prims(std::cout);
     std::cout << std::endl;
 
     switch (argc) {
         case 1:
-            test1(world);
-            test2(world);
-            test3(world);
-            test4(world);
-            test5(world);
+            test1(*world);
+            test2(*world);
+            test3(*world);
+            test4(*world);
+            test5(*world);
             break;
         case 2:
             switch (std::atoi(argv[1])) {
-                case 1: test1(world); break;
-                case 2: test2(world); break;
-                case 3: test3(world); break;
-                case 4: test4(world); break;
-                case 5: test5(world); break;
+                case 1: test1(*world); break;
+                case 2: test2(*world); break;
+                case 3: test3(*world); break;
+                case 4: test4(*world); break;
+                case 5: test5(*world); break;
                 default: throw std::runtime_error("wrong number of test case");
             }
             break;
@@ -154,5 +156,4 @@ int main(int argc, char* argv[]) {
     std::cout << "\n\nworld has expressions: " << std::endl;
     world->dump();
     std::cout << std::endl;
-    delete world;
 }
